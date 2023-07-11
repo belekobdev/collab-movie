8a3c96c7e784e8a2c988572cc8d0fe939fd02808
Azamat <belekovazamat127@gmail.com>
2023-07-11 10:50:11 +0600

    together

---
+++ src/App.tsx
@@ -0,0 +1,54 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { NavLink } from "react-router-dom";
+
+function App() {
+  return (
+    <div className="container">
+      <div className="App">
+        <div className="welcome px-[40px] flex jutify-center py-[100px] flex-col">
+          <h1 className="text-white text-[40px] font-[900]">
+            Добро пожаловать.
+          </h1>
+          <h2 className="text-white text-[30px] font-[500] relative bottom-[10px]">
+            Миллионы фильмов, сериалов и людей. Исследуйте сейчас.
+          </h2>
+          <div className="flex items-center mt-[40px]">
+            <input
+              className="w-[1300px] px-[20px] text-[16px] outline-none h-[45px] rounded-[40px]"
+              placeholder="Найти фильм, сериал, персону....."
+              type="text"
+            />
+            <button className="btn   w-[140px] rounded-[40px] relative right-[40px] h-[45px]">
+              Search
+            </button>
+          </div>
+        </div>
+        <div>
+          <div className="flex items-center mt-[30px]">
+            <h1 className=" ml-[30px] text-[26px]">В тренде</h1>
+            <div className="border">
+              <NavLink
+                className="left  text-[gray]  bg-[white] h-[40px] py-[3px] px-[17px] rounded-[30px] "
+                to={"/popular"}
+              >
+                Popular
+              </NavLink>
+              <NavLink
+                className="right bg-[white] text-[gray] h-[40px] py-[3px]  px-[17px] rounded-[30px]"
+                to={"/topRated"}
+              >
+                Top Rated
+              </NavLink>
+            </div>
+          </div>
+
+          <div className="mt-[20px] ml-[30px]">movies</div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
