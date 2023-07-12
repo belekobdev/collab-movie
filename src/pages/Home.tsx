a2f82386baca3b125eb6552f01e1268b07dbc51f
belekov <116719835+belekobdev@users.noreply.github.com>
2023-07-12 10:49:05 +0600

    darkMode (#12)

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,18 @@
-import React from 'react';
-import { NavLink, useNavigate } from 'react-router-dom';
-import { useAppDispatch } from '../hooks/useAppDispatch';
-import { useAppSelector } from '../hooks/useAppSelector';
-import { settingLanguage, settingSearch } from '../store/Reducers/searchSlice';
-import ru from "../images/rus.png"
-import us from "../images/us.png"
+
+import React from "react";
+import { NavLink, useNavigate } from "react-router-dom";
+import { useAppDispatch } from "../hooks/useAppDispatch";
+import { useAppSelector } from "../hooks/useAppSelector";
+import { darkMode, settingSearch } from "../store/Reducers/searchSlice";
 
 const Home = () => {
+ 
+     const dispatch = useAppDispatch();
+  const { searching } = useAppSelector((s) => s.searchSlice);
+  const { popular } = useAppSelector((s) => s.popularSlice);
+  const { dark } = useAppSelector((s) => s.searchSlice);
+  const nav = useNavigate();
+    const state = Math.floor(Math.random() * 20)
     const dispatch = useAppDispatch();
   const { searching, language} = useAppSelector(s => s.searchSlice);
   const nav = useNavigate();
@@ -35,42 +41,49 @@ const Home = () => {
           </h2>
           <div className="flex items-center mt-[40px]">
             <input onKeyDown={(e) => {
+
               if (e.key === "Enter") {
-                nav(`/search/search_movie/${searching}`)
+                nav(`/search/search_movie/${searching}`);
               }
             }}
-             onChange={(e) => dispatch(settingSearch(e.target.value))}
-              className="w-[1300px] px-[20px] text-[16px] outline-none h-[45px] rounded-[40px]"
-              placeholder="Найти фильм, сериал, персону....."
-              type="text"
-            />
-            <button onClick={() => nav(`/search/search_movie/${searching}`)}
-             className="btn   w-[140px] rounded-[40px] relative right-[40px] h-[45px]">
-              Search
-            </button>
+            onChange={(e) => dispatch(settingSearch(e.target.value))}
+            className="w-[1300px] px-[20px] text-[16px] outline-none h-[45px] rounded-[40px]"
+            placeholder="Найти фильм, сериал, персону....."
+            type="text"
+          />
+          <button
+            onClick={() => nav(`/search/search_movie/${searching}`)}
+            className="btn   w-[140px] rounded-[40px] relative right-[40px] h-[45px]"
+          >
+            Search
+          </button>
+        </div>
+      </div>
+      <div>
+        <div className="flex items-center mt-[30px]">
+          <h1  style={{
+            color: dark ? "white" : "black"
+          }} className=" ml-[30px] text-[26px]">В тренде</h1>
+          <div style={{
+            border: dark ? "1px solid white" : ""
+          }} className="border">
+            <NavLink
+              className="left  text-[gray]  bg-[white] h-[40px] py-[3px] px-[17px] rounded-[30px] "
+              to={"/"}
+            >
+              Popular
+            </NavLink>
+            <NavLink
+              className="right bg-[white] text-[gray] h-[40px] py-[3px]  px-[17px] rounded-[30px]"
+              to={"/topRated"}
+            >
+              Top Rated
+            </NavLink>
           </div>
         </div>
-        <div>
-          <div className="flex items-center mt-[30px]">
-            <h1 className=" ml-[30px] text-[26px]">В тренде</h1>
-            <div className="border">
-              <NavLink
-                className="left  text-[gray]  bg-[white] h-[40px] py-[3px] px-[17px] rounded-[30px] "
-                to={"/"}
-              >
-                Popular
-              </NavLink>
-              <NavLink
-                className="right bg-[white] text-[gray] h-[40px] py-[3px]  px-[17px] rounded-[30px]"
-                to={"/topRated"}
-              >
-                Top Rated
-              </NavLink>
-            </div>
-          </div>
-        </div>
-        </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Home;
--- src/store/Reducers/searchSlice.ts
+++ src/store/Reducers/searchSlice.ts
@@ -4,11 +4,18 @@ import { IMovies } from "../../types/IMovie"
 interface ISearchState {
     search: IMovies[],
     searching: string,
+
+    dark: boolean
+
     language: string
+
 }
 const initialState: ISearchState = {
     search: [],
     searching: "",
+
+    dark: false
+
     language: "en-US"
 }
 export const searchSlice = createSlice({
@@ -21,10 +28,14 @@ export const searchSlice = createSlice({
         settingSearch(state, action: PayloadAction<string>){
             state.searching = action.payload
         },
+        darkMode(state, action:  PayloadAction<boolean>) {
+            state.dark = action.payload
+        },
         settingLanguage(state, action: PayloadAction<string>) {
             state.language = action.payload
         }
     }
 })
 export default searchSlice.reducer
+export const {getSearch, settingSearch, darkMode} = searchSlice.actions
 export const {getSearch, settingSearch, settingLanguage} = searchSlice.actions
